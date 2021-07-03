d0340a795862f0fc577a81e8737f7123d87b6bfc
prajwal <mail2prajwal12@gmail.com>
2021-07-03 19:31:05 +0530

    working random password generator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ifStmt } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,8 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length =0;
+  useLetters= false;
+  useNumbers= false;
+  useSymbols= false;
   password = '';
+ 
   onButtonClick() {
-    this.password = 'Hello World';
+    console.log(`The selected options are
+    Password length: ${this.length}
+    Include letters: ${this.useLetters}
+    Include numbers: ${this.useNumbers}
+    Include symbols: ${this.useSymbols}
+    `);
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars ='';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+     validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i=0; i< this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword +=validChars[index];
+    }
+
+    // finally assigning the password
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
   }
 }
